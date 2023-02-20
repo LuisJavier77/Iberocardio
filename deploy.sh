de21fca8ad32cfda797894e62cb7d107aa543874
Luis Javier <luis.javier@panoimagen.com>
2023-02-20 16:35:31 +0100

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<NOMBRE DE USUARIO>/<NOMBRE DE USUARIO>.github.io.git main
 
 # si estás desplegando en https://<NOMBRE DE USUARIO>.github.io/<REPO>
-git push -f git@github.com:luisjavier77/iberocardio.git main:gh-pages
+git push -f git@github.com:luisjavier77/Iberocardio.git main:gh-pages
 
 cd -
