374b7b5808a697bb6c0427a20dbd0259b9ef216a
Luis Javier <luis.javier@panoimagen.com>
2023-02-20 17:43:26 +0100

    change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import {defineConfig } from 'vite'
+// import {defineConfig } from 'vite'
 
-export default defineConfig({
+// export default defineConfig({
+//     base:'/Iberocardio/'
+// })
+// vite.config.js
+export default {
     base:'/Iberocardio/'
-})
+  }
