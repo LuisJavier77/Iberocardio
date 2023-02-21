54fa8de6562acfaa6b8ce76ec2ad0c909afec46a
Luis Javier <luis.javier@panoimagen.com>
2023-02-21 09:51:52 +0100

    sda

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import Phaser from "/phaser"
+import Phaser from "/Iberocardio/phaser"
 import TitleScreen from './src/scenes/TitleScreen'
 import cardGame from './src/scenes/cardGame'
 import DealCards from './src/scenes/DealCards'
